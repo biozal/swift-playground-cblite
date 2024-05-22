1292f07a2d6200133f9edc166c8341f5952c4953
Aaron LaBeau <alabeau@gmail.com>
2024-05-22 13:56:33 -0500

    add package file for testing

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version:5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "cblite-ee-test",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "// swift-tools-version:5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CouchbaseLiteSwift",
+    platforms: [
+        .iOS(.v12),
+        .macOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "CouchbaseLiteSwift",
+            targets: ["CouchbaseLiteSwift"]),
+    ],
+    targets: [
+        .target(
+            name: "CouchbaseLiteSwift",
+    ])",
+    ]
+)
